d92a186e41d03797adde91decc1f4e475381d262
grogu <jonas.bihr@zitis.bund.de>
2023-11-16 21:32:42 +0100

    Bug fixing in main and changed searchview

--- app/src/main/java/com/rhib/androidsophtemplate/MainActivity.kt
+++ app/src/main/java/com/rhib/androidsophtemplate/MainActivity.kt
@@ -5,36 +5,40 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val TAG = "MAIN_ACTIVITY"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         var dbHelper = DatabaseHelper(this)
-        val searchView = R.id.searchView
-        var addProdButt = R.id.addProductToDatabaseButton
+        var searchView:SearchView = findViewById(R.id.searchView)
+        var addProdButt:Button = findViewById(R.id.addProductToDatabaseButton);
 
-        // Search bar searching
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                Log.d("SEARCHBAR", "onQueryTextSubmit: Debug query -> $query")
-                if (!query.isNullOrBlank()) {
-                    // What happens when search is executed
-                    val products = dbHelper.searchProduct(query)
-                    Log.d("SEARCHBAR", "onQueryTextSubmit: Search was executed")
-                }
-                return true
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                // What happens when text changes, w.g. search suggestions
-                return true
-            }
-        })
+//        // Search bar searching
+//        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+//            override fun onQueryTextSubmit(query: String?): Boolean {
+//                Log.d("SEARCHBAR", "onQueryTextSubmit: Debug query -> $query")
+//                if (!query.isNullOrBlank()) {
+//                    // What happens when search is executed
+//                    val products = dbHelper.searchProduct(query)
+//                    Log.d("SEARCHBAR", "onQueryTextSubmit: Search was executed")
+//                }
+//                return true
+//            }
+//
+//            override fun onQueryTextChange(newText: String?): Boolean {
+//                // What happens when text changes, w.g. search suggestions
+//                return true
+//            }
+//        })
 
         addProdButt.setOnClickListener(View.OnClickListener {
             addProduct()
@@ -44,29 +48,36 @@ class MainActivity : AppCompatActivity() {
     private fun addProduct() {
 
         // Get fields and get values from them
-        val productNameEditText = R.id.productNameEditText
-        val productDescriptionEditText = R.id.productDescriptionEditText
-        val productPriceEditText = R.id.productPriceEditText
+        val productNameEditText:EditText = findViewById(R.id.productNameEditText)
+        val productDescriptionEditText:EditText = findViewById(R.id.productDescriptionEditText)
+        val productPriceEditText:EditText = findViewById(R.id.productPriceEditText)
         val enteredName = productNameEditText.toString()
+        Log.d(TAG, "enteredName: $enteredName")
         val enteredDescription = productDescriptionEditText.toString()
-        val enteredPrice = productPriceEditText.toDouble()
+        Log.d(TAG, "enteredDescription: $enteredDescription")
+        val enteredPrice = productPriceEditText.toString()
+        Log.d(TAG, "enteredPrice: $enteredPrice")
+
+        val enteredPriceDouble = enteredPrice.toDouble()
+        Log.d(TAG, "enteredPriceDouble: $enteredPriceDouble")
+
 
         // Check if all fields are filled
-        if (enteredName.isNotEmpty() && enteredDescription.isNotEmpty()) {
-
-            Log.d(
-                "ADD PRODUCT",
-                "addProduct: " + "name: $enteredName" + "desc: $enteredDescription" + "price $enteredPrice"
-            )
-            // If so, add product to database
-//            val product = DatabaseHelper.Product(enteredName, enteredDescription, enteredPrice)
-//            dbhelper.insertProduct(product)
-
-            // Reset input fields
-
-        } else {
-            Toast.makeText(this, "Values can't be empty!", Toast.LENGTH_SHORT).show()
-        }
+//        if (enteredName.isNotEmpty() && enteredDescription.isNotEmpty()) {
+//
+//            Log.d(
+//                "ADD PRODUCT",
+//                "addProduct: " + "name: $enteredName" + "desc: $enteredDescription" + "price $enteredPrice"
+//            )
+//            // If so, add product to database
+////            val product = DatabaseHelper.Product(enteredName, enteredDescription, enteredPrice)
+////            dbhelper.insertProduct(product)
+//
+//            // Reset input fields
+//
+//        } else {
+//            Toast.makeText(this, "Values can't be empty!", Toast.LENGTH_SHORT).show()
+//        }
 
 
 //    }.setNegativeButton("Exit")
